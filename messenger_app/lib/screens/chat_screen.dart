47c27f2f8e00414df024cd88147a4757f5c3b1d4
ajvelo <>
2019-12-18 17:11:45 +0000

    Add title to chat_screen

--- messenger_app/lib/screens/chat_screen.dart
+++ messenger_app/lib/screens/chat_screen.dart
@@ -11,6 +11,30 @@ class ChatScreen extends StatefulWidget {
 class _ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      appBar: AppBar(
+        title: Text(
+          widget.user.name,
+          style: TextStyle(fontSize: 28.0, fontWeight: FontWeight.bold),
+        ),
+        elevation: 0.0,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.more_horiz),
+            iconSize: 30.0,
+            color: Colors.white,
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(30.0),
+                topRight: Radius.circular(30.0))),
+      ),
+    );
   }
 }
