d134bca0886685c58c775b655c98ceacee26b8c7
ajvelo <>
2019-12-18 11:50:41 +0000

    Add category_selector

--- messenger_app/lib/screens/home_screen.dart
+++ messenger_app/lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:messenger_app/widgets/category_selector.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -8,6 +9,37 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          iconSize: 30.0,
+          color: Colors.white,
+          onPressed: () {
+            print("test menu");
+          },
+        ),
+        title: Text(
+          "Chats",
+          style: TextStyle(fontSize: 28.0, fontWeight: FontWeight.bold),
+        ),
+        elevation: 0.0,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            iconSize: 30.0,
+            color: Colors.white,
+            onPressed: () {
+              print("test search");
+            },
+          ),
+        ],
+      ),
+      body: Column(
+        children: <Widget>[
+          CategorySelector(),
+        ],
+      ),
+    );
   }
 }
