cdddd387f1a722d6fb06f10d31c22b3dc9c5e59c
ajvelo <>
2019-12-18 14:55:37 +0000

    Complete recent_chats

--- messenger_app/lib/widgets/recent_chats.dart
+++ messenger_app/lib/widgets/recent_chats.dart
@@ -13,25 +13,62 @@ class RecentChats extends StatelessWidget {
             topRight: Radius.circular(30.0),
           ),
         ),
-        child: ListView.builder(
-          itemCount: chats.length,
-          itemBuilder: (BuildContext context, int index) {
-            final Message chat = chats[index];
-            return Row(
-              children: <Widget>[
-                CircleAvatar(
-                  radius: 35.0,
-                  backgroundImage: AssetImage(chat.sender.imageUrl),
-                ),
-                Column(
-                  children: <Widget>[
-                    Text(chat.sender.name),
-                    Text(chat.text),
-                  ],
-                ),
-              ],
-            );
-          },
+        child: ClipRRect(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(30.0),
+            topRight: Radius.circular(30.0),
+          ),
+          child: ListView.builder(
+            itemCount: chats.length,
+            itemBuilder: (BuildContext context, int index) {
+              final Message chat = chats[index];
+              return Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      CircleAvatar(
+                        radius: 35.0,
+                        backgroundImage: AssetImage(chat.sender.imageUrl),
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            chat.sender.name,
+                            style: TextStyle(
+                                color: Colors.grey,
+                                fontSize: 15.0,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(
+                            height: 5.0,
+                          ),
+                          Container(
+                            width: MediaQuery.of(context).size.width * 0.5,
+                            child: Text(
+                              chat.text,
+                              style: TextStyle(
+                                  color: Colors.blueGrey,
+                                  fontSize: 15.0,
+                                  fontWeight: FontWeight.w600),
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: <Widget>[Text(chat.time), Text('NEW')],
+                  )
+                ],
+              );
+            },
+          ),
         ),
       ),
     );
