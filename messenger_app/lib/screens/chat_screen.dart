921ada1c02279c0b75d7484882266da96b2efa66
ajvelo <>
2019-12-20 15:36:21 +0000

    Add textfield to chat_screen

--- messenger_app/lib/screens/chat_screen.dart
+++ messenger_app/lib/screens/chat_screen.dart
@@ -67,6 +67,37 @@ class _ChatScreenState extends State<ChatScreen> {
     );
   }
 
+  _buildMessageComposer() {
+    return Container(
+        padding: EdgeInsets.symmetric(horizontal: 8.0),
+        height: 70.0,
+        color: Colors.white,
+        child: Row(
+          children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.photo),
+              iconSize: 25.0,
+              color: Theme.of(context).primaryColor,
+              onPressed: () {},
+            ),
+            Expanded(
+              child: TextField(
+                textCapitalization: TextCapitalization.sentences,
+                onChanged: (value) {},
+                decoration:
+                    InputDecoration.collapsed(hintText: 'Send a message...'),
+              ),
+            ),
+            IconButton(
+              icon: Icon(Icons.send),
+              iconSize: 25.0,
+              color: Theme.of(context).primaryColor,
+              onPressed: () {},
+            )
+          ],
+        ));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -86,33 +117,37 @@ class _ChatScreenState extends State<ChatScreen> {
           )
         ],
       ),
-      body: Column(
-        children: <Widget>[
-          Expanded(
-            child: Container(
-              decoration: BoxDecoration(
-                  color: Colors.white,
+      body: GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(30.0),
+                        topRight: Radius.circular(30.0))),
+                child: ClipRRect(
                   borderRadius: BorderRadius.only(
                       topLeft: Radius.circular(30.0),
-                      topRight: Radius.circular(30.0))),
-              child: ClipRRect(
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(30.0),
-                    topRight: Radius.circular(30.0)),
-                child: ListView.builder(
-                  reverse: true,
-                  padding: EdgeInsets.only(top: 15.0),
-                  itemCount: messages.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    final Message message = messages[index];
-                    final bool isMe = message.sender.id == currentUser.id;
-                    return _buildMessage(message, isMe);
-                  },
+                      topRight: Radius.circular(30.0)),
+                  child: ListView.builder(
+                    reverse: true,
+                    padding: EdgeInsets.only(top: 15.0),
+                    itemCount: messages.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      final Message message = messages[index];
+                      final bool isMe = message.sender.id == currentUser.id;
+                      return _buildMessage(message, isMe);
+                    },
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
+            _buildMessageComposer(),
+          ],
+        ),
       ),
     );
   }
